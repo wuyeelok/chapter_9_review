6e8737bf99d583150bd20b0240b580ec7e243efd
wuyeelok <wuyeelok@gmail.com>
2023-12-20 20:37:01 +0800

    Print out some list

--- src/ques/seventeen/MMMM.java
+++ src/ques/seventeen/MMMM.java
@@ -16,6 +16,7 @@ public class MMMM {
 
 		List<Integer> one = List.of(8, 16, 2);
 		var copy = List.copyOf(one);
+		copy.forEach(System.out::println);
 
 		// list.replaceAll(x -> x * 2);
 		one.replaceAll(x -> x * 2);
